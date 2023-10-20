697df042309fbf066d3d5d76aa089902aff34c49
ZGarry <1160564525@qq.com>
2023-10-20 13:50:10 +0800

    Automatic commit

--- r.py
+++ r.py
@@ -1,4 +1,17 @@
 import git
+# 移动到当前目录
+import os
+
+# 获取当前文件的绝对路径
+current_file = os.path.abspath(__file__)
+
+# 获取当前文件所在的目录路径
+current_directory = os.path.dirname(current_file)
+
+# 将当前工作目录更改为当前文件所在的目录
+os.chdir(current_directory)
+
+
 repo = git.Repo('.')
 
 # 添加所有修改的文件到暂存区
