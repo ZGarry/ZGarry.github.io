84ea96116bad6f0412db825950f618cc18e17283
ZGarry <1160564525@qq.com>
2023-10-24 01:04:35 +0800

    Automatic commit

--- r.py
+++ r.py
@@ -1,25 +1,19 @@
 import git
-# 移动到当前目录
-import os
 
-# 获取当前文件的绝对路径
-current_file = os.path.abspath(__file__)
+def quickPush():
+    # 移动到当前目录
+    import os
+    current_file = os.path.abspath(__file__)
+    current_directory = os.path.dirname(current_file)
+    os.chdir(current_directory)
 
-# 获取当前文件所在的目录路径
-current_directory = os.path.dirname(current_file)
+    # 获取当前仓库&提交代码
+    repo = git.Repo('.')
+    repo.git.add('--all')
+    repo.git.commit('-m', 'Automatic commit')
 
-# 将当前工作目录更改为当前文件所在的目录
-os.chdir(current_directory)
+    # 推送到远程仓库的master分支
+    origin = repo.remote(name='origin')
+    origin.push('master')
 
-
-repo = git.Repo('.')
-
-# 添加所有修改的文件到暂存区
-repo.git.add('--all')
-
-# 提交更改
-repo.git.commit('-m', 'Automatic commit')
-
-# 推送到远程仓库的master分支
-origin = repo.remote(name='origin')
-origin.push('master')
+quickPush()
