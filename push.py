e1262a47f86936be8efcc1b4e8389589a02c7728
ZGarry <1160564525@qq.com>
2023-10-24 01:48:00 +0800

    Automatic commit

--- push.py
+++ push.py
@@ -1,5 +1,8 @@
-import git
-
+def get_date_str():
+    from datetime import datetime
+    now = datetime.now()
+    date_string = f"{now.year}-{now.month:02d}-{now.day:02d}"
+    return date_string
 
 def quickPush():
     # 移动到当前目录
@@ -9,11 +12,13 @@ def quickPush():
     os.chdir(current_directory)
 
     # 获取当前仓库&提交代码
+    import git
     repo = git.Repo('.')
     repo.git.add('--all')
+    # repo.git.commit('-m', get_date_str())
     repo.git.commit('-m', 'Automatic commit')
 
-    # 推送到远程仓库的master分支
+    # 推送到远程仓库的main分支(github默认main，其他工具可能不同)
     origin = repo.remote(name='origin')
     origin.push('master')
 
